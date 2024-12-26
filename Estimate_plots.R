ba76c8d3b8e3183220b6c71421f01b10ea49617f
David Pearce <davepearce5819@gmail.com>
2024-12-26 15:57:03 -0600

    included point count nimble models

--- Estimate_plots.R
+++ Estimate_plots.R
@@ -3,11 +3,20 @@ library(ggplot2)
 
 hds_dat <- readRDS("./Data/Fitted_Models/PC_HDS_Dens.rds")
 nmix_dat<-readRDS("./Data/Fitted_Models/PC_Nmix_Dens.rds")
+tmpEhds_dat <- readRDS("./Data/Fitted_Models/PC_TempEmHDS_Dens.rds")
+cmr_dat <-readRDS("./Data/Fitted_Models/PC_CMR_Dens.rds")
 
-lat_dens_df <- rbind(hds_dat, nmix_dat)
+mean(hds_dat$Density)
+mean(nmix_dat$Density)
+mean(tmpEhds_dat$Density)
+mean(cmr_dat$Density)
+
+# Combine into one dataframe
+dens_df <- rbind(hds_dat, nmix_dat, tmpEhds_dat, cmr_dat)
+head(dens_df)
 
 # Plot
-ggplot(lat_dens_df, aes(x = Model, y = Density, fill = Model)) +
+ggplot(dens_df, aes(x = Model, y = Density, fill = Model)) +
   geom_violin() + 
   geom_boxplot(aes(x = Model, y = Density), 
                width = 0.2, position = position_dodge(width = 0.8)) +
@@ -15,7 +24,7 @@ ggplot(lat_dens_df, aes(x = Model, y = Density, fill = Model)) +
     title = "Latent Density", 
     x = "Model", 
     y = "Density") +
-  scale_y_continuous(limits = c(0, 10), breaks = seq(0, 10, by = 5), labels = scales::comma) + # Customize y-axis
+  scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, by = 5), labels = scales::comma) + # Customize y-axis
   theme_minimal() +
   theme(
     axis.text.x = element_text(size = 12, angle = 45, hjust = 1), # Tilt x-axis text
