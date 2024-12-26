61f6aa5df37f2f1ed9ff5f3b15a70d186c93bf84
David Pearce <davepearce5819@gmail.com>
2024-12-26 16:32:44 -0600

    Added in a mean abundance for Texas

--- Estimate_plots.R
+++ Estimate_plots.R
@@ -1,10 +1,12 @@
 
-library(ggplot2)
+library(tidyverse)
 
 hds_dat <- readRDS("./Data/Fitted_Models/PC_HDS_Dens.rds")
 nmix_dat<-readRDS("./Data/Fitted_Models/PC_Nmix_Dens.rds")
 tmpEhds_dat <- readRDS("./Data/Fitted_Models/PC_TempEmHDS_Dens.rds")
 cmr_dat <-readRDS("./Data/Fitted_Models/PC_CMR_Dens.rds")
+
+head(hds_dat)
 
 # mean density
 mean(hds_dat$Density)
@@ -18,21 +20,38 @@ mean(nmix_dat$Density) * 1096.698
 mean(tmpEhds_dat$Density) * 1096.698
 mean(cmr_dat$Density) * 1096.698
 
+# Typical abundance of quail in Texas is 1-2 birds per acre
+# which would be 2.47 to 4.94 birds per hectare.
+# adding in as a "truth"
+txDens_dat <- as.data.frame(c(2.47, 4.94))
+colnames(txDens_dat)[1] <- "Density"
+txDens_dat$Model <- "Mean Texas"
+txDens_dat <- txDens_dat[, c(2, 1)]# Switch columns
+head(txDens_dat)
+mean(txDens_dat$Density)
 
 # Combine into one dataframe
-dens_df <- rbind(hds_dat, nmix_dat, tmpEhds_dat, cmr_dat)
+dens_df <- rbind(txDens_dat, hds_dat, nmix_dat, tmpEhds_dat, cmr_dat)
 head(dens_df)
+
+
+
+
+# Calculate means for each model
+means <- dens_df %>%
+  group_by(Model) %>%
+  summarize(mean_density = mean(Density))
 
 # Plot
 ggplot(dens_df, aes(x = Model, y = Density, fill = Model)) +
-  geom_violin() + 
-  geom_boxplot(aes(x = Model, y = Density), 
-               width = 0.2, position = position_dodge(width = 0.8)) +
+  geom_violin(trim = FALSE, alpha = 0.7) + 
+  geom_point(data = means, aes(x = Model, y = mean_density), color = "black", size = 3) +
+  geom_hline(yintercept = 3.705, color = "black", linetype = "dashed", size = 1) +  # Add horizontal line
   labs(
-    title = "Latent Density", 
+    title = "Density (Per Hectare)", 
     x = "Model", 
-    y = "Density") +
-  scale_y_continuous(limits = c(0, 20), breaks = seq(0, 20, by = 5), labels = scales::comma) + # Customize y-axis
+    y = "Abundance/Hectare") +
+  scale_y_continuous(limits = c(0, 10), breaks = seq(0, 10, by = 2), labels = scales::comma) + # Customize y-axis
   theme_minimal() +
   theme(
     axis.text.x = element_text(size = 12, angle = 45, hjust = 1), # Tilt x-axis text
@@ -40,5 +59,5 @@ ggplot(dens_df, aes(x = Model, y = Density, fill = Model)) +
     plot.title = element_text(hjust = 0.5), 
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    legend.position = "none" 
+    legend.position = "none"
   )
