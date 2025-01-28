fad623139bd67ba2cd0ee53a716c8b1834c2a9b0
David Pearce <davepearce5819@gmail.com>
2025-01-27 18:02:45 -0600

    Added in 7 day moving average

--- CallsPerDay.R
+++ CallsPerDay.R
@@ -3,9 +3,10 @@
 #                    Load libraries
 #
 # -------------------------------------------------------
-
+ 
 # Load packages
 library(tidyverse)
+library(zoo)
 
 # Set seed, scientific notation options, and working directory
 set.seed(123)
@@ -22,7 +23,6 @@ setwd(".")
 bnet_dat <- read.csv("./Data/Acoustic_Data/ARU_BirdNET_alldates.csv") # 2024 4/28 - 8/8
 
 # bnet_dat <- read.csv("./Data/Acoustic_Data/ARU_BirdNET_2023.csv") # 2023 6/15 - 7/11 
-# 
 # bnet_dat <- bnet_dat[which( bnet_dat$Common_name == "Northern Bobwhite"),]
 
 # -------------------------------------------------------
@@ -30,6 +30,11 @@ bnet_dat <- read.csv("./Data/Acoustic_Data/ARU_BirdNET_alldates.csv") # 2024 4/2
 #                    Data Wrangling
 #
 # -------------------------------------------------------
+
+
+# -------------------------
+# Daily Count
+# --------------------------
 
 # Adding a count to summarize calls by day
 bnet_dat$Count <- 1
@@ -44,11 +49,6 @@ call_sum <- bnet_dat %>%
   summarize(Total_Calls = sum(Count), .groups = "drop")
 print(call_sum)
 
-# -------------------------------------------------------
-#
-#                        Plots
-#
-# -------------------------------------------------------
 
 
 # Plot the barplot
@@ -66,3 +66,29 @@ ggplot(call_sum, aes(x = Date, y = Total_Calls)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+
+# -------------------------
+# Moving window
+# --------------------------
+
+# Calculate the 7-day moving average
+call_sum <- call_sum %>%
+  mutate(Moving_Avg = zoo::rollmean(Total_Calls, k = 7, fill = NA, align = "center")) # Requires zoo package
+
+# Plot 
+ggplot(call_sum, aes(x = Date)) +
+  geom_line(aes(y = Total_Calls), color = "black", size = 1, alpha = 0.7) +  # Original calls
+  geom_line(aes(y = Moving_Avg), color = "red", size = 1.2) +                # 7-day moving average
+  labs(
+    title = "Total Calls Per Day with 7-Day Moving Average",
+    x = "Date",
+    y = "Number of Calls"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+
+
+# -------------------------
+# Within 30 recording
+# --------------------------
