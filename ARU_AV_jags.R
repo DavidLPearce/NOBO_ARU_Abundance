23bfcd6b5392938e88925ca9c0fc5fb945e7bfb9
David Pearce <davepearce5819@gmail.com>
2025-01-30 21:42:49 -0600

    Model runs and the fit is good. Need to run model with actual calls validated.

--- ARU_AV_jags.R
+++ ARU_AV_jags.R
@@ -62,6 +62,9 @@ setwd(".")
 # BirdNet detections
 bnet_dat_all <- read.csv("./Data/Acoustic_Data/ARU_BirdNET_alldates.csv")
 
+# Site covariates
+site_covs <- read.csv("./Data/Acoustic_Data/ARU_siteCovs.csv")
+
 
 # -------------------------------------------------------
 #
@@ -113,11 +116,22 @@ print(v)
                              
 # ARU at site 24 stopped recording on 6/20. Making Columns 8:14 NA
 v[24,8:14] <- NA
-print(v)                                 
+print(v)  
+
+# Renaming columns to date Month_day
+formatted_dates <- format(as.Date(date_order), "%b_%d")
+colnames(v) <- formatted_dates
+print(v) 
+
+# Adding rownames
+rownames(v) <- as.numeric(1:27)
 
 # Get the site numbers with at least one call
-sites.a <- which(apply(v, 2, function(col) any(col > 0)))
+v_mat <- as.matrix(v)
+rownames(v_mat) <- NULL  
+sites.a <- which(rowSums(!is.na(v_mat) & v >= 1) > 0)
 print(sites.a)
+
 
 
 # Creating a binary detection matrix, values >= 1 become 1, and 0 stays 0
@@ -141,58 +155,50 @@ J <- rep(14, R)
 # Manually validated info ****** made up ----- Fix this 
 # ---------------------------------------
 
-# sum(v, na.rm = TRUE)
-# 27*14
-# calls2eval<- sample(860, size = (378*.3), replace = FALSE)
-# write.csv(v, "v.csv")
+#write.csv(v, "v.csv")
 
-# Need to redo this - i think not all sites were validated 
-n <- read.csv("./totalval_fake.csv") # n = total number of manually validated acoustic vocalizations 
-k <- read.csv("./totaltrue_fake.csv") # k = true number of manually validated acoustic vocalizations
+# calls validated, do not include sites with no calls
+# only include occasions where at least 1 call was validated for a site
+n <- as.matrix(read.csv("./n.csv", row.names = 1))
 
-# Convert to a matrix and numeric
-n <- as.matrix(n)
-k <- as.matrix(k)
+# calls found to be true, same dimension as n
+k <- as.matrix(read.csv("./k.csv", row.names = 1))
 
-# set row and col names to default ([,1], [,2])
-rownames(n) <- NULL  
-colnames(n) <- NULL  
-rownames(k) <- NULL  
-colnames(k) <- NULL 
+# Survey dates call was validated, same dimension as n
+# should be the column number 
+val.times  <- as.matrix(read.csv("val.times.csv", row.names = 1))
 
 # Total number of sites with manually validated data
-# R.val <- sum(apply(n, 1, function(row) any(row >= 1)))
-# print(R.val) # used as R.val = number of validated sites for acoustic data
-R.val = R
+R.val <- nrow(n)
 
-# ---------------------------------------
-# Covariates ****** made up
-# ---------------------------------------
-set.seed(123)
-
-# Generate a random vector for 27 sites (e.g., between 0 and 100)
-random_covariate <- runif(R, min = 0, max = 100)
-
-# Scale the covariate between 0 and 1 using min-max scaling
-X.lambda <- (random_covariate - min(random_covariate)) / (max(random_covariate) - min(random_covariate))
-
-# convert to matrix
-X.lambda <- as.matrix(X.lambda)
-
-# Display the scaled covariate
-print(X.lambda)
+# How many surveys were validate
+J.val <- rep(14, R.val)  
 
 
 # ---------------------------------------
+# Covariates *** site covs made up ***
+# ---------------------------------------
 
-# J.r contains the number of surveys at each acoustic site that contains
-# at least 1 detected vocalization. 
-J.r <- apply(v, 1, function(a) {sum(a != 0)})
+# Extract site covariates
+herbPdens <- as.vector(scale(site_covs[,c("herb_Pdens")])) 
+woodyParea <- as.vector(site_covs[,c("woody_Parea")]) 
+
+# For day random effect on cue production rate
+days <- matrix(NA, nrow = 27, ncol = 14)  
+for (col in 1:14) {
+  days[, col] <- col
+}
+print(days)
+
+
+
+# J.r contains the number of surveys at each acoustic site that contains at least 1 detected vocalization. 
+J.r <- apply(v_mat, 1, function(a) {sum(a != 0)})
 J.r <- ifelse(is.na(J.r), 0, J.r)
 J.r <- as.numeric(J.r)
+print(J.r)
 
-
-# A.times is a n.sites x J matrix that contains the indices of the
+# A.times is a R x J matrix that contains the indices of the
 # specific surveys at each acoustic site that have at least one detected
 # vocalization. This determines the specific components of v[i, j] that
 # are used in the zero-truncated Poisson.
@@ -203,18 +209,9 @@ for (i in 1:R) {
     A.times[i, 1:length(tmp[[i]])] <- tmp[[i]]
   }
 }
-
-# How many valid observations across visits
-J.val <- apply(v[sites.a, ], 1, function(a){sum(!is.na(a))})
+print(A.times)
 
 
-
-# All times are valid (no NAs from survey failure)
-val.times <- matrix(rep(1:4, times = nrow(v)), nrow = nrow(v), byrow = TRUE)
-
-
-# For day random effect on cue production rate
-days <- matrix(rep(1:4, times = nrow(v)), nrow = nrow(v), byrow = TRUE)
 
 # For Bayesian P-value
 R.A <- sum(J.r > 0)
@@ -225,7 +222,6 @@ J.A <- max(J)
 # Bundle data 
 bugs.data.A <- list(R = R, 
                     J = J, 
-                    X.lambda = X.lambda, 
                     v = v, 
                     y = y, 
                     k = k, 
@@ -239,63 +235,27 @@ bugs.data.A <- list(R = R,
                     R.A = R.A, 
                     J.A = J.A, 
                     sites.a.v = sites.a.v, 
-                    n.sites = R / 3, 
                     days = days,
-                    n.days = max(days, na.rm = TRUE))
+                    n.days = max(J),
+                    herbPdens = herbPdens,
+                    woodyParea = woodyParea)
 
 str(bugs.data.A)
 
 
-# Initial Values 
-N.init <- rep(1, R)
-inits <- function() {
-  list(
-    N = N.init, 
-    beta.0 = rnorm(1),
-    beta.1 = rnorm(1), 
-    omega = runif(1, 0, 10), 
-    tau.day = runif(1, 0.1, 1),
-    tau = runif(1, 0.1, 1),
-    tau.p = runif(1, 0.1, 1),
-    tau.day.p = runif(1, 0.1, 1),
-    alpha.1 = runif(1, 0, 1), 
-    a.phi = runif(1, 0, 5)
-  )
-}
 
 
-
-# Parameters monitored
-params.A <- c('alpha.0', 
-              'alpha.1', 
-              'beta.0',
-              'beta.1', 
-              'tau', 
-              'tau.day', 
-              #'a.phi', 
-              'omega', 
-              'bp.y', 
-              'bp.v',
-              'lambda')
-
-# MCMC settings 
-n.iter <- 200000
-n.thin <- 50
-n.burn <- 60000
-n.chain <- 3
-n.adapt <- 5000
-
-
-
-# Model Statement 
-model_AV <- "
-model {
+# -------------------------------------------------------
+# Acoustic Model
+# -------------------------------------------------------
+cat(" model {
   
   # -------------------------------------------
   # Priors 
   # -------------------------------------------
   beta.0 ~ dnorm(0, .01)
   beta.1 ~ dnorm(0, .01)
+  beta.2 ~ dnorm(0, .01)
   alpha.0 <- logit(mu.alpha)
   mu.alpha ~ dunif(0, 1)
   alpha.1 ~ dunif(0, 1000) # Constrained to be positive
@@ -316,16 +276,23 @@ model {
   # -------------------------------------------
   # Likelihood and process model 
   # -------------------------------------------
-  
   for (i in 1:R) {
-    log(lambda[i]) <- beta.0 + beta.1 * X.lambda[i, 1]
+  
+    # Abundance Model
+    log(lambda[i]) <- beta.0 + beta.1 * herbPdens[i] + beta.2 * woodyParea[i]
     N[i] ~ dpois(lambda[i])
+    
+    # Detection Model
     logit(p.a[i]) <- alpha.0 + alpha.1 * N[i]
-    # Acoustic Data -------------------
+    
+    # Acoustic Data 
     for (j in 1:J[i]) {
+    
+    # Vocalization Model
       log(delta[i, j]) <- gamma.1[days[i, j]]
       y[i, j] ~ dbin(p.a[i], 1)
       tp[i, j] <- delta[i, j] * N[i] / (delta[i, j] * N[i] + omega)
+      
       # Posterior predictive checks for Bayesian P-value
       y.pred[i, j] ~ dbin(p.a[i], 1)
       resid.y[i, j] <- pow(pow(y[i, j], 0.5) - pow(p.a[i], 0.5), 2)
@@ -344,7 +311,6 @@ model {
   # ------------------------------------------- 
   # Manual validation 
   # -------------------------------------------
-  
   for (i in 1:R.val) {
     for (j in 1:J.val[i]) {
       K[i, j] ~ dbin(tp[sites.a[i], j], v[sites.a[i], val.times[i, j]])
@@ -355,7 +321,6 @@ model {
   # -------------------------------------------
   # Bayesian P-value
   # -------------------------------------------
-  
   for (i in 1:R.A) {
     tmp.v[i] <- sum(resid.v[sites.a.v[i], 1:J.r[sites.a.v[i]]])
     tmp.v.pred[i] <- sum(resid.v.pred[sites.a.v[i], 1:J.r[sites.a.v[i]]])
@@ -367,35 +332,75 @@ model {
   bp.y <- step(fit.y.pred - fit.y)
   bp.v <- step(fit.v.pred - fit.v)
 }
-"
-# Write the model to a temporary file
-model_file <- tempfile(pattern = "model_", fileext = ".txt")
-writeLines(model_AV, con = model_file)
+", fill=TRUE, file="./jags_models/ARU_mod0.txt")
+# ------------End Model-------------
 
-# Run Model
-out.model.A <- jags(
-  data = bugs.data.A, 
-  inits = inits, 
-  parameters.to.save = params.A, 
-  model.file = model_file, 
-  n.iter = n.iter, 
-  n.thin = n.thin, 
-  n.burnin = n.burn, 
-  n.chains = n.chain, 
-  n.adapt = n.adapt, 
-  parallel = TRUE
-)
 
+
+# Parameters monitored
+params.A <- c('alpha.0', 
+              'alpha.1', 
+              'beta.0',
+              'beta.1',
+              'beta.2',
+              'tau', 
+              'tau.day', 
+              'a.phi', 
+              'omega', 
+              'bp.y', 
+              'bp.v',
+              'lambda')
+
+# Initial Values 
+N.init <- rep(1, R)
+inits <- function() {
+  list(
+    N = N.init, 
+    beta.0 = rnorm(1),
+    beta.1 = rnorm(1), 
+    beta.2 = rnorm(1),
+    omega = runif(1, 0, 10), 
+    tau.day = runif(1, 0.1, 1),
+    tau = runif(1, 0.1, 1),
+    tau.p = runif(1, 0.1, 1),
+    tau.day.p = runif(1, 0.1, 1),
+    alpha.1 = runif(1, 0, 1), 
+    a.phi = runif(1, 0, 5)
+  )
+}#end inits
+
+
+
+
+# Fit Model
+fm.0 <- jags(data = bugs.data.A, 
+                    inits = inits, 
+                    parameters.to.save = params.A, 
+                    model.file = "./jags_models/ARU_mod.txt", 
+                    n.iter = 200000, 
+                    n.burnin = 60000, 
+                    n.chains = 3,
+                    n.thin = 50,
+                    n.adapt = 5000, 
+                    parallel = TRUE,
+                    n.cores = 8,
+                    DIC = TRUE) 
+                    
 
 # Rhat
-coda::gelman.diag(out.model.A$samples)
-str(out.model.A$samples)
+fm.0$Rhat
+
+# Model summary
+print(fm.0, digits = 3)
+
 # Trace plots
-mcmcplot(out.model.A$samples)
+mcmcplot(fm.0$samples)
 
-# Model Summary
-summary(out.model.A$samples)
+# Bayesian P value
+cat("Bayesian p-value =", fm.0$summary["bp.v",1], "\n")
 
+# Average number of false positives detections
+cat("False positives =", fm.0$summary["omega",1], "\n")
 
 #  -------------------------------------------------------
 #
@@ -444,7 +449,7 @@ ggplot(dens_df, aes(x = Model, y = Density, fill = Model)) +
     title = "Latent Density",
     x = "Model",
     y = "Density") +
-  scale_y_continuous(limits = c(0, 10),
+  scale_y_continuous(limits = c(0, 20),
                      breaks = seq(0, 10, by = 5),
                      labels = scales::comma) +
   theme_minimal() +
@@ -474,6 +479,6 @@ HCI_dens * 1096.698
 
 
 # Export Density data frame
-saveRDS(dens_df, "./Data/Fitted_Models/ARU_Bnet.rds")
+#saveRDS(dens_df, "./Data/Fitted_Models/ARU_Bnet.rds")
 
 
