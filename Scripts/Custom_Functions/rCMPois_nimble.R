406404afef452889ef3c8fb1cd1f582bc9b37dc8
David Pearce <davepearce5819@gmail.com>
2025-03-21 19:03:45 -0500

    Renamed

--- Scripts/Custom_Functions/rCMPois_nimble.R
+++ Scripts/Custom_Functions/rCMPois_nimble.R
@@ -1,8 +1,8 @@
 # Conway-Maxwell Poisson random values function
-rCMPois <- nimbleRcall(
+rCMPois_nimble <- nimbleRcall(
   function(n=double(0), lambda=double(0), nu=double(0)){},
   Rfun = 'rcmp',
   returnType=double()) 
 
 # Define in global environment
-assign('rCMPois', rCMPois, envir=.GlobalEnv)
+assign('rCMPois_nimble', rCMPois_nimble, envir=.GlobalEnv)
--- Scripts/Custom_Functions/register_Conway-Maxwell_Poisson_distribution.R
+++ Scripts/Custom_Functions/register_Conway-Maxwell_Poisson_distribution.R
@@ -1,9 +1,9 @@
 # Register Conway-Maxwell Poisson function
 registerDistributions(
   list(
-    dCMPois = list(   
-      BUGSdist = "dCMPois(lambda, nu)",  
-      Rdist = "dCMPois(lambda, nu)",  
+    dCMPois_nimble = list(   
+      BUGSdist = "dCMPois_nimble(lambda, nu)",  
+      Rdist = "dCMPois_nimble(lambda, nu)",  
       types = c('value = double(0)', 'lambda = double(0)', 'nu = double(0)'),
       pqAvail = FALSE
     )
