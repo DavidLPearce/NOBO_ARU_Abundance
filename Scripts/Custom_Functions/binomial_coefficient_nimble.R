960f0f117bccac2ba7b955b52116a5117652aa64
David Pearce <davepearce5819@gmail.com>
2025-04-07 14:21:17 -0500

    No longer using

--- Scripts/Custom_Functions/binomial_coefficient_nimble.R
+++
@@ -1,9 +0,0 @@
-# Binomial Coefficient Function
-choose_nimble <- nimbleFunction(
-  run = function(n = double(0), k = double(0)) {  # Removed extra `)`
-    returnType(double(0))
-    if (k > n) return(0)
-    if (k < 0) return(0)
-    return(exp(logFactorial(n) - logFactorial(k) - logFactorial(n - k)))
-  }
-)
